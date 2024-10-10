af78c80bd8035c19d71c54ee658678c97eda5141
Kato-Trevor <katotrevor99@gmail.com>
2024-10-10 21:58:59 +0300

    added addProduct integration test

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,7 @@
 import '@testing-library/jest-dom';
 import { jest } from '@jest/globals';
+import { PrismaClient } from '@prisma/client'
+import { mockDeep } from 'jest-mock-extended'
 
 global.TextEncoder = require('util').TextEncoder;
 global.TextDecoder = require('util').TextDecoder;
@@ -10,3 +12,8 @@ jest.mock('@/db/db', () => ({
       delete: jest.fn(),
     },
   }));
+
+jest.mock('@/db/db', () => ({
+  __esModule: true,
+  default: mockDeep<PrismaClient>(),
+}))
